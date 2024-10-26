76f57687e49bea8293c312e538cb1126eb7eae48
OscarMinjarez <minjarez.dev@gmail.com>
2024-10-26 15:09:11 -0700

    Presentation project added

---
+++ BattleshipPresentation/src/main/java/org/itson/battleshippresentation/BattleshipPresentation.java
@@ -0,0 +1,8 @@
+package org.itson.battleshippresentation;
+
+public class BattleshipPresentation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
