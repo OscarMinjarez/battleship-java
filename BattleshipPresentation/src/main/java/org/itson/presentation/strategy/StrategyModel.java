c0eac96872cb3a72f6b7cae695cde1a2f89b9028
Yalam <Yalam>
2024-11-21 18:17:37 -0600

    refactoring to Model-View-ViewModel in process

--- BattleshipPresentation/src/main/java/org/itson/presentation/strategy/StrategyModel.java
+++ BattleshipPresentation/src/main/java/org/itson/presentation/strategy/StrategyModel.java
@@ -3,43 +3,59 @@ package org.itson.presentation.strategy;
 
 import domain.Player;
 import domain.Ship;
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.*;
+
 
 /**
  *
  * @author PabloCeasxr
  */
 public class StrategyModel {
+    private Map<String, Integer> shipsAvailable;
+    private java.util.List<IStrategyObserver> observers;
 
-    private Player player;
-    private static StrategyModel instance;
-    private IStrategyObserver iStrategyObserver;
-
-    private StrategyModel() {
-
+    public StrategyModel() {
+        shipsAvailable = new HashMap<>() {
+            {
+                put("Aircraft carriers", 2);  // 2 portaaviones
+                put("Cruisers", 2);  // 2 cruceros
+                put("Submarines", 4);  // 4 submarinos
+                put("Ships", 3);  // 3 barcos de 1 celda
+            }
+        };
+        observers = new ArrayList<>();
     }
 
-    public void update() {
+    public void addObserver(IStrategyObserver observer) {
+        observers.add(observer);
     }
-    
-    public static StrategyModel getInstance() {
-        if (instance == null) {
-            instance = new StrategyModel();
+
+    public void removeObserver(IStrategyObserver observer) {
+        observers.remove(observer);
+    }
+
+    private void notifyObservers() {
+        for (IStrategyObserver observer : observers) {
+            observer.update();
         }
-        return instance;
     }
 
-    public void setPlayerReady(boolean value) {
-
+    public Map<String, Integer> getShipsAvailable() {
+        return shipsAvailable;
     }
 
-    public void putShipOnTable(Ship ship) {
-
+    public void placeShip(String shipType) {
+        if (shipsAvailable.containsKey(shipType) && shipsAvailable.get(shipType) > 0) {
+            shipsAvailable.put(shipType, shipsAvailable.get(shipType) - 1);
+            notifyObservers();  // Notificar a los observadores sobre los cambios
+        }
     }
 
-    private boolean verifyPosition() {
-        return true;
+    public boolean isShipAvailable(String shipType) {
+        return shipsAvailable.getOrDefault(shipType, 0) > 0;
     }
-
-    
-
 }
